645ba43ef3b37349934e3f8c63dd6bd30eaaede1
Christian <lalanne@gmail.com>
2013-05-26 21:04:00 +0200

    * ADD: first version of square root

--- sqrt.py
+++ sqrt.py
@@ -1,8 +1,9 @@
 
-x = 4.
+x = 9.
 s = 1.
 
 for k in range(6):
+    print "before iteration %s, s = %s" % (k,s)
     s = 0.5 * (s + x/s)
 
-print "result: %f" % s 
+print "after %s iterationsi, s = %s" % (k+1,s) 
