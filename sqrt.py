2431e0e13bade685a9e05d9e2a6df5ee9ff0923e
Christian <lalanne@gmail.com>
2013-05-26 23:29:35 +0200

    * CHG: sqrt2 became a function to be used as a module

--- sqrt.py
+++ sqrt.py
@@ -1,18 +1,17 @@
 
-x = 10000.
-s = 1.
-kmax = 100
-tolerance = 1.e-14
+def sqrt2(x):
+    s = 1.
+    kmax = 100
+    tolerance = 1.e-14
 
-for k in range(kmax):
-    print "before iteration %s, s = %s" % (k,s)
-    s0 = s;
-    s = 0.5 * (s + x/s)
-    delta = s - s0
-    if abs(delta/x) < tolerance:
-        break
-
-print "after %s iterations, s = %s" % (k+1,s) 
+    for k in range(kmax):
+        print "before iteration %d, s = %20.15f" % (k,s)
+        s0 = s;
+        s = 0.5 * (s + x/s)
+        delta = s - s0
+        if abs(delta/x) < tolerance:
+            break
+    print "after %s iterations, s = %20.15f" % (k+1,s) 
 
 
 
