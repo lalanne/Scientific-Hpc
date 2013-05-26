e14b0f96b9bc15e4b5944c7356f11e516df52385
Christian <lalanne@gmail.com>
2013-05-26 23:13:42 +0200

    * ADD: break protocol based on tolerance number

--- sqrt.py
+++ sqrt.py
@@ -1,12 +1,18 @@
 
-x = 9.
+x = 10000.
 s = 1.
+kmax = 100
+tolerance = 1.e-14
 
-for k in range(6):
+for k in range(kmax):
     print "before iteration %s, s = %s" % (k,s)
+    s0 = s;
     s = 0.5 * (s + x/s)
+    delta = s - s0
+    if abs(delta/x) < tolerance:
+        break
 
-print "after %s iterationsi, s = %s" % (k+1,s) 
+print "after %s iterations, s = %s" % (k+1,s) 
 
 
 
