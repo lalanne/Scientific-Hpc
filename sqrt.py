3bb45c34920370e18eb720b7d9c8e35ff77a0285
Christian <lalanne@gmail.com>
2013-06-02 18:29:03 +0200

    * ADD: adding debug mode and real return of square root

--- sqrt.py
+++ sqrt.py
@@ -2,7 +2,7 @@
 Module to calculate root square approximation
 using Newton's method.
 """
-def sqrt2(x):
+def sqrt2(x, debug=False):
     """
     function that calculates root square approximation
     using Newton's method.
@@ -12,13 +12,16 @@ def sqrt2(x):
     tolerance = 1.e-14
 
     for k in range(kmax):
-        print "before iteration %d, s = %20.15f" % (k,s)
-        s0 = s;
+        if debug:
+            print "before iteration %d, s = %20.15f" % (k,s)
+        s0 = s
         s = 0.5 * (s + x/s)
         delta = s - s0
         if abs(delta/x) < tolerance:
             break
-    print "after %s iterations, s = %20.15f" % (k+1,s) 
-
+    if debug:
+        print "after %s iterations, s = %20.15f" % (k+1,s) 
+    
+    return s
 
 
